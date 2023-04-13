22b77921fe8794a3a2f5a7ee7fa1ca653cda21ef
SERGE3-g <gueaserge2@gmail.com>
2023-04-13 10:33:50 +0200

    new file

---
+++ Blog-serge/main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+	
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1){  
+			$('.page-title').addClass("sticky");
+		}
+		else{
+			$('.page-title').removeClass("sticky");
+		}
+	});
+
+});
+
+
+
+
